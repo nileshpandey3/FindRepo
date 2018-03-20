0234dcbf8c1132973f2b990e2b9051f09df9e241
Nilesh <nileshpandey3@gmail.com>
2018-03-19 22:47:02 -0700

    created 2 screens and succesfully retreived names of repos in listview

---
+++ app/src/main/java/com/example/nileshpandey/findrepo/GithubApi.kt
@@ -0,0 +1,47 @@
+package com.example.nileshpandey.findrepo
+
+import android.app.Service
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Retrofit
+import retrofit2.http.GET
+import retrofit2.http.Query
+import retrofit2.converter.gson.GsonConverterFactory
+
+/**
+ * Created by nileshpandey on 3/19/18.
+ */
+
+
+interface GithubService{
+
+    @GET("search/repositories?")
+    fun searchRepos(@Query("q") searchTerm:String) : Call<GithubSearchResults>
+}
+
+class GithubSearchResults(val items: List<Repo>)
+class Repo(val full_name:String, val html_url:String)
+class  GithubUser(val avatar_url:String)
+
+class GithubRetreiver{
+
+    val service:GithubService
+
+    init {
+        val retroFit = Retrofit.Builder().baseUrl("https://api.github.com").addConverterFactory(GsonConverterFactory.create()).build()
+
+        service = retroFit.create(GithubService::class.java)
+    }
+
+    fun searchRepos(callback: Callback<GithubSearchResults>, searchTerm: String){
+
+        var searchT = searchTerm
+        if (searchT==""){
+
+            searchT = "Eggs"
+        }
+        val call = service.searchRepos(searchT)
+        call.enqueue(callback)
+
+    }
+}
--- app/src/main/java/com/example/nileshpandey/findrepo/search_result_activity.kt
+++ app/src/main/java/com/example/nileshpandey/findrepo/search_result_activity.kt
@@ -2,6 +2,11 @@ package com.example.nileshpandey.findrepo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import retrofit2.Call
+import retrofit2.Response
+
 
 class search_result_activity : AppCompatActivity() {
 
@@ -10,6 +15,31 @@ class search_result_activity : AppCompatActivity() {
         setContentView(R.layout.activity_search_result_activity)
 
         val searchTerm = intent.getStringExtra("SearchTerm")
-        println(message = searchTerm)
+
+        val callback = object : retrofit2.Callback<GithubSearchResults>{
+            override fun onFailure(call: Call<GithubSearchResults>?, t: Throwable?) {
+               println("Not working")
+            }
+
+            override fun onResponse(call: Call<GithubSearchResults>?, response: Response<GithubSearchResults>?) {
+                val searchResult = response?.body()
+
+                if (searchResult!= null){
+
+                    for (Repo in searchResult!!.items){
+
+                        println(Repo.full_name)
+                    }
+
+                    val listView = findViewById<ListView>(R.id.repoListView)
+                    val adapter = ArrayAdapter(this@search_result_activity,android.R.layout.simple_list_item_1, searchResult!!.items)
+                    listView.adapter = adapter
+                }
+            }
+
+
+        }
+        var retriever = GithubRetreiver()
+        retriever.searchRepos(callback,searchTerm)
     }
 }
