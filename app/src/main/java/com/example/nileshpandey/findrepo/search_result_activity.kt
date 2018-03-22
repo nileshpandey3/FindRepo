fd7cd7d424743075b6456ecb8919a76f0e07368d
Nilesh <nileshpandey3@gmail.com>
2018-03-21 20:56:24 -0700

    Added a custom RepoAdapter instead of ArrayAdapter to display the name of the repo in a list

--- app/src/main/java/com/example/nileshpandey/findrepo/search_result_activity.kt
+++ app/src/main/java/com/example/nileshpandey/findrepo/search_result_activity.kt
@@ -1,9 +1,14 @@
 package com.example.nileshpandey.findrepo
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import android.widget.TextView
 import retrofit2.Call
 import retrofit2.Response
 
@@ -32,7 +37,7 @@ class search_result_activity : AppCompatActivity() {
                     }
 
                     val listView = findViewById<ListView>(R.id.repoListView)
-                    val adapter = ArrayAdapter(this@search_result_activity,android.R.layout.simple_list_item_1, searchResult!!.items)
+                    val adapter = RepoAdapter(this@search_result_activity,android.R.layout.simple_list_item_1, searchResult!!.items)
                     listView.adapter = adapter
                 }
             }
@@ -42,4 +47,26 @@ class search_result_activity : AppCompatActivity() {
         var retriever = GithubRetreiver()
         retriever.searchRepos(callback,searchTerm)
     }
+}
+
+class RepoAdapter(context: Context?, resource: Int, objects: List<Repo>?) : ArrayAdapter<Repo>(context, resource, objects) {
+
+    override fun getCount(): Int {
+        return super.getCount()
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+
+        val inflator = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+        val repoView = inflator.inflate(android.R.layout.simple_list_item_1, parent, false) as TextView
+
+        val repo = getItem(position)
+
+        repoView.text = repo.full_name
+
+        return repoView
+
+    }
+
 }
